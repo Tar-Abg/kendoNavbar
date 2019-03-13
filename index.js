f179fb195ff53b7a29184858dfe50ca0893b2796
Taron Abgaryan <taronabg@mail.ru>
2019-03-13 17:44:51 +0400

    create ul by jquery

--- index.js
+++ index.js
@@ -2,54 +2,54 @@ import * as $ from "jquery";
 require ("@progress/kendo-ui/js/kendo.menu.js");
 
 export function createNavabr(rootElement){
-    var navBar = document.createElement("ul");
-    rootElement.appendChild(navBar); 
-    $("ul").kendoMenu({
-        dataSource: [
-        {
-            text: "Model",
-            items: [
+    rootElement.append(
+        $("<ul>").kendoMenu({
+            dataSource: [
             {
-                text: "Bmw",
+                text: "Model",
                 items: [
-                { text: "BMW X1" },
-                { text: "BMW 3 Series GT" },
-                { text: "BMW i8" },
-                { text: "BMW X4" },
-                { text: "BMW X6" },
-                ]
+                {
+                    text: "Bmw",
+                    items: [
+                    { text: "BMW X1" },
+                    { text: "BMW 3 Series GT" },
+                    { text: "BMW i8" },
+                    { text: "BMW X4" },
+                    { text: "BMW X6" },
+                    ]
+                },
+                {
+                    text: "Mercedes-Benz",
+                    items: [
+                    { text: "GLA 45 AMG" },
+                    { text: "AMG GT" },
+                    { text: "GLE" },
+                    { text: "GLE 2019" },
+                    ],
+                }
+                ],
+                cssClass: "forFont"
             },
             {
-                text: "Mercedes-Benz",
+                text: "German Cars",
                 items: [
-                { text: "GLA 45 AMG" },
-                { text: "AMG GT" },
-                { text: "GLE" },
-                { text: "GLE 2019" },
+                { text: "Audi" },
+                { text: "Opel" },
+                { text: "Volkswagen" },
+                { text: "Ford-Werke GmbH" }
                 ],
+            },
+            {
+                text: "Contact",
+                cssClass: "forFont",
+                url: "#"
+            },
+            {
+                text: "About",
+                cssClass: "forFont",
+                url: "#grid"
             }
             ],
-            cssClass: "forFont"
-        },
-        {
-            text: "German Cars",
-            items: [
-            { text: "Audi" },
-            { text: "Opel" },
-            { text: "Volkswagen" },
-            { text: "Ford-Werke GmbH" }
-            ],
-        },
-        {
-            text: "Contact",
-            cssClass: "forFont",
-            url: "#"
-        },
-        {
-            text: "About",
-            cssClass: "forFont",
-            url: "#grid"
-        }
-        ],
-    });
+        })
+    )
 }
