229629e3ad6d5ab0b40243f4788f13336275a54d
Taron Abgaryan <taronabg@mail.ru>
2019-03-17 19:43:32 +0400

    some chenges

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ require ("@progress/kendo-ui/js/kendo.menu.js");
 export function createNavabr(rootElement, dataForMenu){
     rootElement.append(
         $("<ul>").kendoMenu({
-            dataSource: {
-                data: dataForMenu
-            }
+            dataSource: dataForMenu
         })
     )
 }
