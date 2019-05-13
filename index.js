996b855183b6e733ddb8594e4bafc8bd043746ea
Tar-Abg <46677613+Tar-Abg@users.noreply.github.com>
2019-05-13 19:40:55 +0400

    code correction

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ export function createNavabr(rootElement, dataForMenu){
         }).addClass("first-menu")
     )
 };
-export function createSecndNavabr(rootElement, dataForMenu){
+export function createSecondNavabr(rootElement, dataForMenu){
     rootElement.append(
         $("<ul>").kendoMenu({
             dataSource: dataForMenu,
             orientation:"horizontal"
-        }).addClass("secnd-menu")
+        }).addClass("second-menu")
     )
 }
